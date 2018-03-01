06c33d3737d18bf993e0892453961ec31f44b9f2
Emma Fronberg <efronberg@mills.edu>
2018-03-01 18:15:43 +0000

    static page for StoryCraft

---
+++ StoryCraftTestProject1/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/home' # the url
+    # get 'static_pages/home', as: '/' # the url
+    # get 'static_pages/home', as: '/home' # the url
+  root :to => redirect('static_pages/home') # home and the other are different
+    # root :to => redirect('/') # home and the other are different
+    # root :to => redirect('/home') # home and the other are different
+end
